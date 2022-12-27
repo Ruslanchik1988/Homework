62f5cbf5f7e232a3ea48e2b0be1f20edd5f52d13
Ruslan <kirito7379@gmail.com>
2022-12-27 19:41:05 +0300

    task27

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a;
+Console.Write($"Введите число ");
+int.TryParse(Console.ReadLine()!, out a);
+int b = a;
+int sum = 0;
+while(b > 0)
+{
+    sum += b % 10;
+    b /= 10;
+}
+Console.Write($"{a} = {sum}");
+
