897bf4eadd46bf546417e20667f9b0cf0ec52879
Ruslan <kirito7379@gmail.com>
2022-12-29 19:12:21 +0300

    task34

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
-﻿int[] a = new int[8];
-for( int b = 0; b < a.Length; b++)
-{
-    a[b] = new Random().Next(9);
-    Console.Write($"{a[b]} - ");
-}
+﻿int[] array = new int[5];
+for(int i = 0; i < array.Length; i ++)
+    array[i] = new Random().Next(100, 1000);
+    int c = 0;
+for(int i = 0; i < array.Length; i ++)
+    if(array[i] % 2 == 0)
+    c++;
+    for(int i = 0; i < array.Length; i ++)
+Console.Write($"{array[i]} ");
+Console.Write($"= {c}");
